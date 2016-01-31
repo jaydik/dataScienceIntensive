23c2489f7779c55af8ecb6bce9c84639592b6373
Jon Dickerson <jaydik@gmail.com>
2016-01-30 20:58:10 -0500

    Add shell script to download data

---
+++ project/getData.sh
@@ -0,0 +1,3 @@
+curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/genres.list.gz
+curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/release-dates.list.gz
+curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/writers.list.gz
