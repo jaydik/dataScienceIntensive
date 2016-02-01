9bf4fe8a39c1dd00df3bbfdb6599c557a79b1e46
Jon Dickerson <jaydik@gmail.com>
2016-01-31 20:28:54 -0500

    Parse titles, release dates, and writers. Still getting some garbage on the writer type.

--- project/getData.sh
+++ project/getData.sh
@@ -1,3 +1,46 @@
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/genres.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/release-dates.list.gz
-curl -O ftp://ftp.fu-berlin.de/pub/misc/movies/database/writers.list.gz
+#!/bin/bash
+
+# HEAVILY influenced by Brandon Rhodes pandas python tutorial
+# available at https://github.com/brandon-rhodes/pycon-pandas-tutorial
+
+FILE="data/genres.list.gz"
+
+if [ -e "$FILE" ]
+then
+    echo "Already got genres"
+else
+    echo "Need genres"
+    curl ftp://ftp.fu-berlin.de/pub/misc/movies/database/genres.list.gz -o data/genres.list.gz
+fi
+
+FILE="data/release-dates.list.gz"
+
+if [ -e "$FILE" ]
+then
+    echo "Already got release dates"
+else
+    echo "Need release dates"
+    curl ftp://ftp.fu-berlin.de/pub/misc/movies/database/release-dates.list.gz -o data/release-dates.list.gz
+fi
+
+FILE="data/writers.list.gz"
+
+if [ -e "$FILE" ]
+then
+    echo "Already got writers"
+else
+    echo "Need writers"
+    curl ftp://ftp.fu-berlin.de/pub/misc/movies/database/writers.list.gz -o data/writers.list.gz
+fi
+
+FILE="data/ratings.list.gz"
+
+if [ -e "$FILE" ]
+then
+    echo "Already got ratings"
+else
+    echo "Need ratings"
+    curl ftp://ftp.fu-berlin.de/pub/misc/movies/database/ratings.list.gz -o data/ratings.list.gz
+fi
+
+python ./build.py
