136bdadffdf59f248a826aa398edba2121d8533f
jonDesktop <jdickerson@musicalfs.com>
2016-03-15 11:20:12 -0400

    Finish SVM

--- ud120-projects/svm/svm_author_id.py
+++ ud120-projects/svm/svm_author_id.py
@@ -28,3 +28,27 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+model = SVC(kernel='rbf', C=10000)
+
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+model.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = model.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+
+print "Accuracy is {0}".format(accuracy_score(pred, labels_test))
+
+print(pred[10], pred[26], pred[50])
+
+print(sum(pred))
+
+
+
