71e624ddd5aa185a8991a9fd67a8edc5a844fbc0
jdickerson <jaydik@gmail.com>
2016-03-18 15:19:31 -0400

    Add feature selection UD120

--- ud120-projects/text_learning/vectorize_text.py
+++ ud120-projects/text_learning/vectorize_text.py
@@ -41,20 +41,32 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            output = []
+            for word in text.split():
+                if word in ['sara', 'shackleton', 'chris', 'germani']:
+                    continue
+                else:
+                    output.append(word)
 
             ### append the text to word_data
+            word_data.append(' '.join(output))
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == 'sara':
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
 
             email.close()
@@ -67,9 +79,11 @@ pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
-
-
-
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+tf_idf = TfidfVectorizer(stop_words='english')
 
+tf_matrix = tf_idf.fit_transform(word_data)
+print len(tf_idf.get_feature_names())
+print tf_idf.get_feature_names()[34597]
